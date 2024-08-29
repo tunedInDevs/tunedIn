0819b76182b325d3cba2250a2e2066ab125ac8fe
Ethan <ethanherrera@berkeley.edu>
2024-08-28 23:12:34 -0700

    migrated the cdk from using ec2 to using ecs on top of ec2 and added secrets manager

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,112 +1,97 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { Construct } from 'constructs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create a VPC
-    const vpc = new ec2.Vpc(this, 'MyVPC', {
-      maxAzs: 2,
-      natGateways: 0,
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: 'Public',
-          subnetType: ec2.SubnetType.PUBLIC,
-        }
-      ]
-    });
-
-    // Create a security group
-    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
-      vpc,
-      description: 'Allow SSH (TCP port 22) in',
-      allowAllOutbound: true
-    });
-
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access')
-
-    // Create an EC2 instance
-    const ec2Instance = new ec2.Instance(this, 'EC2Instance', {
-      vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      machineImage: new ec2.AmazonLinuxImage({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
-      }),
-      userData: ec2.UserData.custom(`
-        #!/bin/bash
-        yum update -y
-        amazon-linux-extras install docker
-        service docker start
-        usermod -a -G docker ec2-user
-        chkconfig docker on
-
-        # Install Docker Compose
-        curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        chmod +x /usr/local/bin/docker-compose
-
-        # Create docker-compose.yml
-        cat <<EOT > /home/ec2-user/docker-compose.yml
-        version: '3.8'
-
-        services:
-          app:
-            image: tunedindev/tunedinbackend:latest
-            ports:
-              - "8080:8080"
-            environment:
-              - SPRING_PROFILES_ACTIVE=local
-              - SPRING_DATASOURCE_URL=jdbc:sqlite:/app/tuned_users.db
-              - SPRING_DATASOURCE_DRIVER_CLASS_NAME=org.sqlite.JDBC
-              - SPRING_JPA_DATABASE_PLATFORM=org.hibernate.community.dialect.SQLiteDialect
-              - SPRING_JPA_HIBERNATE_DDL_AUTO=update
-              - SPRING_JPA_PROPERTIES_HIBERNATE_DIALECT=org.hibernate.community.dialect.SQLiteDialect
-              - SPRING_JPA_PROPERTIES_HIBERNATE_HBM2DDL_AUTO=update
-              - SPRING_JPA_SHOW_SQL=true
-              - SPOTIFY_CLIENT_ID=\${SPOTIFY_CLIENT_ID}
-              - SPOTIFY_CLIENT_SECRET=\${SPOTIFY_CLIENT_SECRET}
-              - SPOTIFY_REDIRECT_URI=\${SPOTIFY_REDIRECT_URI}
-              - SPOTIFY_API_BASE_URL=\${SPOTIFY_API_BASE_URL}
-            volumes:
-              - ./data:/app/data
-        EOT
-
-        # Create and run the startup script
-        cat <<EOT > /home/ec2-user/start-app.sh
-        #!/bin/bash
-        # Load environment variables
-        source /home/ec2-user/.env
-        # Navigate to the directory containing docker-compose.yml
-        cd /home/ec2-user
-        # Pull the latest image and start the container
-        docker-compose pull
-        docker-compose up -d
-        EOT
-
-        chmod +x /home/ec2-user/start-app.sh
-
-        # Create a placeholder .env file (you'll need to populate this with real values)
-        touch /home/ec2-user/.env
-
-        # Create the data directory
-        mkdir -p /home/ec2-user/data
-
-        # Run the startup script
-        /home/ec2-user/start-app.sh
-      `),
-      securityGroup: securityGroup,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC
+    // Create a secret for the Spotify client ID
+    const clientId = new secretsmanager.Secret(this, 'SpotifyClientId', {
+      secretName: 'spotify-client-id',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ CLIENT_ID: 'default-client-id' }),
+        generateStringKey: 'CLIENT_ID',
       },
     });
 
-    ec2Instance.connections.allowFromAnyIpv4(ec2.Port.tcp(8080));
+    // Create a secret for the Spotify client secret
+    const clientSecret = new secretsmanager.Secret(this, 'SpotifyClientSecret', {
+      secretName: 'spotify-client-secret',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ CLIENT_SECRET: 'default-client-secret' }),
+        generateStringKey: 'CLIENT_SECRET',
+      },
+    });
 
-    // Output the public IP address
-    new cdk.CfnOutput(this, 'InstancePublicIp', {
-      value: ec2Instance.instancePublicIp
+    // Create a VPC
+    const vpc = new ec2.Vpc(this, 'TunedInVPC', {
+      maxAzs: 2
+    });
+
+    // Create an ECS cluster
+    const cluster = new ecs.Cluster(this, 'TunedInCluster', {
+      vpc: vpc
+    });
+
+    // Add capacity to the cluster
+    const autoScalingGroup = cluster.addCapacity('DefaultAutoScalingGroup', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      desiredCapacity: 1,
+      maxCapacity: 1, // Limit to 1 instance for free tier
+      minCapacity: 1,
+    });
+
+    // Create a task definition
+    const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TunedInBackendTask');
+    const container = taskDefinition.addContainer('tunedInBackend', {
+      image: ecs.ContainerImage.fromRegistry("tunedindev:tunedinbackend"),
+      memoryLimitMiB: 512,
+      cpu: 256,
+      portMappings: [{ containerPort: 8080 }],
+      secrets: {
+        SPOTIFY_CLIENT_ID: ecs.Secret.fromSecretsManager(clientId, 'CLIENT_ID'),
+        SPOTIFY_CLIENT_SECRET: ecs.Secret.fromSecretsManager(clientSecret, 'CLIENT_SECRET'),
+      },
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'TunedInBackend' }),
+    });
+
+    // Grant the task role permissions to read the secrets
+    clientId.grantRead(taskDefinition.taskRole);
+    clientSecret.grantRead(taskDefinition.taskRole);
+
+    // Create a service
+    const service = new ecs.Ec2Service(this, 'TunedInBackendService', {
+      cluster,
+      taskDefinition,
+      desiredCount: 1,
+    });
+
+    // Create an Application Load Balancer
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LoadBalancer', {
+      vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    listener.addTargets('ECS', {
+      port: 80,
+      targets: [service],
+      healthCheck: {
+        path: '/',
+        interval: cdk.Duration.seconds(60),
+      }
+    });
+
+    // Output the load balancer DNS name
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
+      value: lb.loadBalancerDnsName
     });
   }
 }
